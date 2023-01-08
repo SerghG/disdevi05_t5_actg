54bd13a3bcd2c7d833421d12a1bc10fa5c811fd2
SerghG <sergi.rubsal.97@gmail.com>
2023-01-08 12:53:53 +0100

    Añadidas pantallas

--- disdevi05_t5_actg/Assets/Scripts/CargarPartidas.cs
+++ disdevi05_t5_actg/Assets/Scripts/CargarPartidas.cs
@@ -8,4 +8,29 @@ public class CargarPartidas : MonoBehaviour
     public void CargarNivel() {
         SceneManager.LoadScene(1);
     }
+
+    public void CargarNivel1() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CargarNivel2() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void PantallaPerdido() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PantallaPrincipal() {
+        SceneManager.LoadScene(4);
+    }
+
+    public void PantallaControles() {
+        SceneManager.LoadScene(5);
+    }
+
+    public void SalirJuego(){
+        Debug.Log("Salir del juego");
+        Application.Quit();
+    }
 }
